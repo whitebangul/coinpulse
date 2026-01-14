401c3c7ff4c87d57a3a6acba0f982c246670ee59
whitebangul <whitebangul0819@gmail.com>
2026-01-14 00:44:48 -0800

    implement nav

---
+++ components/Header.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const header = () => {
+  const pathname = usePathname();
+  return (
+    <header>
+      <div className="main-container inner">
+        <Link href="/">
+          <Image
+            src="logo.svg"
+            alt="CoinPulse logo"
+            width={132}
+            height={40}
+          ></Image>
+        </Link>
+
+        <nav>
+          <Link
+            href="/"
+            className={cn("nav-link", {
+              "is-active": pathname == "/",
+              "is-home": true,
+            })}
+          >
+            Home
+          </Link>
+          <p>Search Modal</p>
+
+          <Link
+            href="/coins"
+            className={cn("nav-link", {
+              "is-active": pathname == "/coins",
+              "is-home": true,
+            })}
+          >
+            All Coins
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default header;
